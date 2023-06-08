67f7e16456e7cc789cf7e03af7d6a3fec51779b2
Monica Monteiro <monicamo@gmail.com>
2023-06-08 20:53:15 -0300

    Entendendo o fluxo de dados

--- pensamentos/src/app/app.module.ts
+++ pensamentos/src/app/app.module.ts
@@ -5,15 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CreateThoughtComponent } from './components/thoughts/create-thought/create-thought.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CreateThoughtComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ pensamentos/src/app/components/thoughts/create-thought/create-thought.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-thought',
+  templateUrl: './create-thought.component.html',
+  styleUrls: ['./create-thought.component.scss']
+})
+export class CreateThoughtComponent {
+
+  pensamento = {
+    id: "1",
+    conteudo: "Aprendendo Angular",
+    autoria: "Dev",
+    modelo: "modelo1"
+  }
+
+  onSave() {
+    alert(" novo pensamenti")
+  }
+
+  onCancel() {
+    alert("cancel")
+  }
+
+}
